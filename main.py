3c5abb489118710895990dac07c0c6bc0919fd6f
NOAH DINAN <91911303+shinysocks7@users.noreply.github.com>
2021-10-08 15:29:08 +0000

    renamed variables

--- main.py
+++ main.py
@@ -4,35 +4,38 @@
 import pygame
 from sys import exit
 pygame.init()
-win_width, win_length = 1100, 750
+win_width, win_length = 700, 600
 win = pygame.display.set_mode((win_width, win_length), pygame.RESIZABLE)
 pygame.display.set_caption("Racoonundrum - a trashy game")
 clock = pygame.time.Clock()
 
 # Sprite Art
 raccoon_image = pygame.image.load("assets/raccoon.png")
+raccoon_image = pygame.transform.scale(raccoon_image, (40, 40))
 
 # Classes
 
 
-class Surf:
+class Level:
+    pass
+
+
+class MazeSurf:
     def __init__(self):
-        self.cube_image_1st = pygame.Surface((400, 400), pygame.SRCALPHA)  # cubesurf
-        self.cube_image = self.cube_image_1st
-        self.cube_image.fill((200, 100, 39))
-        self.cube_rect = self.cube_image.get_rect(center=[win_width/2, win_length/2])
+        self.surf1 = pygame.Surface((400, 400), pygame.SRCALPHA)
+        self.surf = self.surf1
+        self.surf.fill((200, 100, 39))
+        self.win_center = [win_width/2, win_length/2]
+        self.surf_rect = self.surf.get_rect(center=self.win_center)
         self.angle = 0
 
-    def resize(self):
-        self.cube_rect = self.cube_image.get_rect(center=[win_width / 2, win_length / 2])
-
     def rotate(self, angle):
-        self.cube_image = pygame.transform.rotozoom(self.cube_image_1st, self.angle, 1)
+        self.surf = pygame.transform.rotozoom(self.surf1, self.angle, 1)
         self.angle += angle
-        self.resize()
+        self.surf_rect = self.surf.get_rect(center=self.win_center)
 
     def update(self):
-        win.blit(self.cube_image, self.cube_rect)
+        win.blit(self.surf, self.surf_rect)
 
         key = pygame.key.get_pressed()
         if key[pygame.K_a]:
@@ -41,9 +44,9 @@ class Surf:
             self.rotate(-2)
 
 
-cube = Surf()
-cube_rect = cube.cube_rect
-cube_surf = cube.cube_image
+maze = MazeSurf()
+surf_rect = maze.surf_rect
+surf = maze.surf
 
 
 class MazeBlock:
@@ -52,20 +55,20 @@ class MazeBlock:
         self.y = y * 40
         self.size = 40
         self.color = (255, 0, 0)
-        self.surf =  cube_surf
+        self.surf =  surf
 
     def update(self):
         pygame.draw.rect(self.surf, self.color, (self.x, self.y, self.size, self.size))
 
 
-class FreeFalling:
+class Raccoon:
     def __init__(self, image):
         self.image = image
         self.image_rect = self.image.get_rect(center=[400, 0])
-        self.cube_rect = cube_rect
-        self.pos_y = self.cube_rect.y
+        self.maze_rect = surf_rect
+        self.pos_y = self.maze_rect.y
         self.speed_y = 0
-        self.gravity_value = 9.8
+        self.gravity_value = 0
 
     def gravity(self):
         self.speed_y += self.gravity_value/60
@@ -78,9 +81,9 @@ class FreeFalling:
 
 
 # Objects
-raccoon = FreeFalling(raccoon_image)
-block1 = MazeBlock(1, 1)
-block2 = MazeBlock(9, 3)
+block1 = MazeBlock(7, 5)
+block2 = MazeBlock(7, 6)
+raccoon = Raccoon(raccoon_image)
 
 # Game Loop
 while True:
@@ -90,11 +93,11 @@ while True:
             pygame.quit()
             exit()
         elif event.type == pygame.VIDEORESIZE:
-            cube.resize()
-            print("boop")
+            win_width, win_length = win_width, win_length
+            cube.win_center = [win_width/2, win_length/2] # fix this
 
     win.fill((255, 255, 255))
-    cube.update()
+    maze.update()
     raccoon.update()
     block1.update()
     block2.update()
